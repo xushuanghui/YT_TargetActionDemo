937adbf23c7dc8b0cab7619137fce7021de9213e
xushuanghui <shuanghuixu@ShuangHuideMacBook-Pro.local>
2017-08-23 18:28:39 +0800

    add a pushView

---
+++ YT_TargetAction/MDPushView.h
@@ -0,0 +1,13 @@
+//
+//  MDPushView.h
+//  YT_TargetAction
+//
+//  Created by shuanghui xu on 2017/8/23.
+//  Copyright © 2017年 yehot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDPushView : UIView
+
+@end
---
+++ YT_TargetAction/MDPushView.m
@@ -0,0 +1,21 @@
+//
+//  MDPushView.m
+//  YT_TargetAction
+//
+//  Created by shuanghui xu on 2017/8/23.
+//  Copyright © 2017年 yehot. All rights reserved.
+//
+
+#import "MDPushView.h"
+
+@implementation MDPushView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
